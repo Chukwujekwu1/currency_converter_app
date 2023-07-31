871ca8a43186498f4cc7ed132746b5d0c416c572
officialmihalis <ongurd5@gmail.com>
2023-07-31 01:36:00 -0700

    hhh

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -5,12 +5,63 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final border = OutlineInputBorder(
+      borderSide: const BorderSide(
+        color: Colors.black,
+        width: 2.0,
+        style: BorderStyle.solid,
+        strokeAlign: BorderSide.strokeAlignInside,
+      ),
+      borderRadius: BorderRadius.circular(5),
+    );
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('0'),
+            const Text(
+              '0',
+              style: TextStyle(
+                fontSize: 45,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: TextField(
+                style: const TextStyle(
+                  color: Colors.black,
+                ),
+                decoration: InputDecoration(
+                  hintText: "Enter amount in USD",
+                  hintStyle: const TextStyle(
+                    color: Colors.black,
+                  ),
+                  prefixIcon: const Icon(
+                    Icons.monetization_on_outlined,
+                  ),
+                  prefixIconColor: Colors.black,
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: border,
+                  enabledBorder: border,
+                ),
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Click me"),
+            ),
+            TextButton(
+                onPressed: () {
+                  
+                },
+                child: const Text('Click me'))
           ],
         ),
       ),
