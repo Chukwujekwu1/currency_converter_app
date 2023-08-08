ab962d581b34ee66b9e6e7b2ec5497ae27e75856
officialmihalis <ongurd5@gmail.com>
2023-08-08 12:31:22 -0700

    finished app

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,7 @@ import 'package:currency_converter/currency_converter_material_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() {  runApp(const MyApp());
 }
 
 
